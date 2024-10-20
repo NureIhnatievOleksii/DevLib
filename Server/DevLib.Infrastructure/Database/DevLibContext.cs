914ebe8a6d07738729e4cf4d407fe3d887ed8fa9
Oleksii Ihnatiev <oleksii.ihnatiev@nure.ua>
2024-10-20 17:28:35 +0300

    Added Post table

--- Server/DevLib.Infrastructure/Database/DevLibContext.cs
+++ Server/DevLib.Infrastructure/Database/DevLibContext.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using DevLib.Domain.CustomerAggregate;
 using DevLib.Domain.UserAggregate;
+using DevLib.Domain.PostAggregate;
 
 namespace DevLib.Infrastructure.Database;
 
 public class DevLibContext(DbContextOptions<DevLibContext> options) : IdentityDbContext<User, IdentityRole<Guid>, Guid>(options)
-{  
+{
     public DbSet<Customer> Customers { get; set; }
+    public DbSet<Post> Posts { get; set; } 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -21,6 +23,27 @@ public class DevLibContext(DbContextOptions<DevLibContext> options) : IdentityDb
             entity.Property(c => c.Id)
                   .ValueGeneratedOnAdd()
                   .HasDefaultValueSql("NEWID()");
+        });
+
+        modelBuilder.Entity<Post>(entity =>
+        {
+            entity.HasKey(p => p.PostId);
+
+            entity.Property(p => p.PostId)
+                  .ValueGeneratedOnAdd()
+                  .HasDefaultValueSql("NEWID()");
+
+            entity.Property(p => p.Title)
+                  .IsRequired()
+                  .HasMaxLength(255);
+
+            entity.Property(p => p.Text)
+                  .IsRequired();
+
+            entity.HasOne(p => p.User)
+                  .WithMany(u => u.Posts)
+                  .HasForeignKey(p => p.UserId)
+                  .OnDelete(DeleteBehavior.Cascade);  
         });
     }
 }
