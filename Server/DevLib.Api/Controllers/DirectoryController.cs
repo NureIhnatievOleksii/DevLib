5941a9a2496db8da4eb50f47c22027a794d625b1
AlexdChal <shurshurych2005@gmail.com>
2024-10-25 15:01:24 +0300

    Added DirectoryController Added API request Post-directory

---
+++ Server/DevLib.Api/Controllers/DirectoryController.cs
@@ -0,0 +1,18 @@
+﻿using DevLib.Application.CQRS.Commands.Directories.CreateDirectories;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevLib.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class DirectoryController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateDirectory([FromBody, Required]  CreateDirectoryCommand command, CancellationToken cancellationToken)
+        {
+            await mediator.Send(command, cancellationToken);
+            return Ok();
+        }
+    }
+}
---
+++ Server/DevLib.Application/CQRS/Commands/Directories/CreateDirectories/CreateDirectoryCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace DevLib.Application.CQRS.Commands.Directories.CreateDirectories;
+
+public record CreateDirectoryCommand(string DirectoryName, string DirectoryImgUrl, List<ArticleDto> Articles) : IRequest;
+
+// todo oc delete dto
+public record ArticleDto(
+    string Name,
+    string Text
+);
