762387c53f3f0afea3a08eba188f44e950852107
Oleksii Ihnatiev <oleksii.ihnatiev@nure.ua>
2024-10-29 15:22:00 +0200

    Added API request for granting moderator and admin permissions.

---
+++ Server/DevLib.Api/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using DevLib.Application.CQRS.Commands.Admins.AssignAdminRole;
+using DevLib.Application.CQRS.Commands.Admins.AssignModeratorRole;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevLib.Api.Controllers
+{
+    [Route("api/auth")]
+    public class AdminController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("assign-moderator-role")] 
+        public async Task<IActionResult> AssignModeratorRole([FromBody] AssignModeratorRoleCommand command, CancellationToken cancellationToken)
+        {
+            var result = await mediator.Send(command, cancellationToken); 
+
+            if (result.IsSuccess)
+            {
+                return Ok(new { Message = "Moderator role assigned successfully." });
+            }
+
+            return BadRequest(result.ErrorMessage);
+        }
+        [HttpPost("assign-admin-role")]
+        public async Task<IActionResult> AssignAdminRole([FromBody] AssignAdminRoleCommand command, CancellationToken cancellationToken)
+        {
+            var result = await mediator.Send(command, cancellationToken);
+
+            if (result.IsSuccess)
+            {
+                return Ok(new { Message = "Admin role assigned successfully." });
+            }
+
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+}
---
+++ Server/DevLib.Application/CQRS/Commands/Admins/AssignModeratorRole/AssignModeratorRoleCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using DevLib.Domain.UserAggregate;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace DevLib.Application.CQRS.Commands.Admins.AssignModeratorRole
+{
+    public class AssignModeratorRoleCommandHandler(UserManager<User> userManager, RoleManager<IdentityRole<Guid>> roleManager
+) : IRequestHandler<AssignModeratorRoleCommand, CommandResultDto>
+    {
+        public async Task<CommandResultDto> Handle(AssignModeratorRoleCommand request, CancellationToken cancellationToken)
+        {
+            var user = await userManager.FindByIdAsync(request.UserId.ToString());
+            if (user == null)
+            {
+                return new CommandResultDto(false, "User not found.");
+            }
+
+            const string roleName = "Moderator";
+
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole<Guid>(roleName));
+            }
+
+            var currentRoles = await userManager.GetRolesAsync(user);
+            var removeResult = await userManager.RemoveFromRolesAsync(user, currentRoles);
+            if (!removeResult.Succeeded)
+            {
+                return new CommandResultDto(false, "Failed to remove existing roles.");
+            }
+
+            var result = await userManager.AddToRoleAsync(user, roleName);
+
+            return result.Succeeded
+                ? new CommandResultDto(true)
+                : new CommandResultDto(false, "Failed to assign moderator role.");
+        }
+    }
+
+
+}
