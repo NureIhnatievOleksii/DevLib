aafbea410a062ddd4aa83f3665b0e2aadaa99f19
Oleksii Ihnatiev <oleksii.ihnatiev@nure.ua>
2024-10-30 17:31:11 +0200

    TagRepository method that adds a tag connection, by two keys, bookId, TagId

---
+++ Server/DevLib.Application/Interfaces/Repositories/ITagRepository.cs
@@ -0,0 +1,12 @@
+﻿using DevLib.Domain.TagAggregate;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevLib.Application.Interfaces.Repositories
+{
+    public interface ITagRepository
+    {
+        Task AddTagConnectionAsync(Guid bookId, Guid tagId, CancellationToken cancellationToken);
+        Task AddTagConnectionToDirectoryAsync(Guid directoryId, Guid tagId, CancellationToken cancellationToken);
+    }
+}
---
+++ Server/DevLib.Infrastructure/Repositories/TagRepository.cs
@@ -0,0 +1,48 @@
+﻿using DevLib.Application.Interfaces.Repositories;
+using DevLib.Domain.TagAggregate;
+using DevLib.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevLib.Infrastructure.Repositories
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly DevLibContext _context;
+
+        public TagRepository(DevLibContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddTagConnectionAsync(Guid bookId, Guid tagId, CancellationToken cancellationToken)
+        {
+            var tagConnection = new TagConnection
+            {
+                TagId = tagId,
+                BookId = bookId,
+                TagConnectionId = Guid.NewGuid()
+            };
+
+            await _context.TagConnections.AddAsync(tagConnection, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task AddTagConnectionToDirectoryAsync(Guid directoryId, Guid tagId, CancellationToken cancellationToken)
+        {
+            var tagConnection = new TagConnection
+            {
+                TagId = tagId,
+                DirectoryId = directoryId, // Присвоение идентификатора каталога
+                TagConnectionId = Guid.NewGuid()
+            };
+
+            await _context.TagConnections.AddAsync(tagConnection, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        // Другие методы...
+    }
+}
