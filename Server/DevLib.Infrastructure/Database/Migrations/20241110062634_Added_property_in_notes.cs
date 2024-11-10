905b450047403a162c04463fb16588f3f4c965fc
Oleksii Ihnatiev <oleksii.ihnatiev@nure.ua>
2024-11-10 08:27:56 +0200

    Fix migration

---
+++ Server/DevLib.Infrastructure/Database/Migrations/20241110062634_Added_property_in_notes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevLib.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_property_in_notes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Notes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId1",
+                table: "Comment",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_UserId1",
+                table: "Comment",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_AspNetUsers_UserId1",
+                table: "Comment",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_AspNetUsers_UserId1",
+                table: "Comment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comment_UserId1",
+                table: "Comment");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Comment");
+        }
+    }
+}
