f1fbf63c3624575f7549bd6ec639bc40057447d2
KolesnykOleksandr <surikostarcraft2@gmail.com>
2024-12-04 00:27:54 +0200

    Added API Delete Dictionary

---
+++ Server/DevLib.Application/CQRS/Commands/Directories/DeleteDirectory/DeleteDirectoryCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevLib.Application.CQRS.Commands.Directories.DeleteDirectory;
+
+public record DeleteDirectoryCommand(Guid Id) : IRequest<IdentityResult>;
