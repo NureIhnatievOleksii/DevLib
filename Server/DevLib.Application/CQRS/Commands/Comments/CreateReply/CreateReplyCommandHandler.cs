30ba9153b709ce119bbe26ac12f5a74636330610
AlexdChal <shurshurych2005@gmail.com>
2024-12-03 18:50:15 +0200

    Fixed bugs related to comments and replies

--- Server/DevLib.Application/CQRS/Commands/Comments/CreateReply/CreateReplyCommandHandler.cs
+++ Server/DevLib.Application/CQRS/Commands/Comments/CreateReply/CreateReplyCommandHandler.cs
@@ -8,26 +8,30 @@ using Microsoft.AspNetCore.Identity;
 using DevLib.Application.CQRS.Commands.Tags.DeleteTagsFromBook;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
+using DevLib.Domain.UserAggregate;
 
 namespace DevLib.Application.CQRS.Commands.Comments
 {
     public class CreateReplyCommandHandler : IRequestHandler<CreateReplyCommand, IdentityResult>
     {
         private readonly ICommentRepository _commentRepository;
+        private readonly UserManager<User> _userManager;
 
-        public CreateReplyCommandHandler(ICommentRepository commentRepository)
+        public CreateReplyCommandHandler(ICommentRepository commentRepository, UserManager<User> userManager)
         {
             _commentRepository = commentRepository;
+            _userManager = userManager;
         }
 
         public async Task<IdentityResult> Handle(CreateReplyCommand command, CancellationToken cancellationToken)
-        {
+        { 
             var comment = new Comment
             {
                 CommentId = Guid.NewGuid(),
                 UserId = command.UserId,
                 Content = command.Text,
-                DateTime = DateTime.UtcNow
+                DateTime = DateTime.UtcNow,
+                User = await _userManager.FindByIdAsync(command.UserId.ToString())
             };
 
             return await _commentRepository.CreateReply(comment, command.CommentId, cancellationToken);
